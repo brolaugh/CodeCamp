69b0a0bf89f73bd7455c500b1a3fb4c5653776de
Brolaugh <haki1003@gmail.com>
2015-07-05 01:00:10 +0200

    Modified .gitignore and added example config.php

--- comp/config/config.php
+++ comp/config/config.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set("Europe/Amsterdam");
 
-define("DB_HOST", "");
-define("DB_NAME", "");
-define("DB_USER", "");
-define("DB_PASS", "");
+define("DB_HOST", "localhost");
+define("DB_NAME", "brolaugh_codecamp");
+define("DB_USER", "brolaugh_me");
+define("DB_PASS", "kattjävel5");
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $mysqli->set_charset("utf8");
