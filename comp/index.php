30ad06aff8dff43bba06adb6ed95549e69d08f04
Brolaugh <haki1003@gmail.com>
2015-07-06 01:26:42 +0200

    moved some code around to make the code easier to read.

--- comp/index.php
+++ comp/index.php
@@ -20,11 +20,19 @@
 <script type="text/javascript" src="scripts/splash.js"></script>
 </head>
 <body onload="init();">
+	<div id="splash">
+		<ul>
+			<li><a href="javascript:void(0); showLoginForm()" class="btn btn-default btn-raised">Login</a></li>
+			<li><a href="javascript:void(0); showRegForm()" class="btn btn-default btn-raised">Register</a></li>
+		</ul>
+			<?php
+				require_once('templates/main-login.php');
+				require_once('templates/main-register.php');
+			?>
+		<a href="forgot-password">forgot your password?</a>
+	</div>
 	<?php
-	//For a 100vh*100vw non moving background
-	require_once('templates/main-splash.php');
 	require_once('templates/main-footer.php');
-
 	?>
 </body>
 </html>
