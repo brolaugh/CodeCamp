b14946191eab6236d27135090e52f81d7559a81a
Brolaugh <haki1003@gmail.com>
2015-07-07 23:56:01 +0200

    Made the splash page elements align correctly

--- comp/index.php
+++ comp/index.php
@@ -22,14 +22,14 @@
 <body onload="init();">
 	<div id="splash">
 		<ul>
-			<li><a href="javascript:void(0); showLoginForm()" class="btn btn-default btn-raised">Login</a></li>
-			<li><a href="javascript:void(0); showRegForm()" class="btn btn-default btn-raised">Register</a></li>
+			<li class="form-choice"><a href="javascript:void(0); showLoginForm()" class="btn btn-default btn-raised">Login</a></li>
+			<li class="form-choice"><a href="javascript:void(0); showRegForm()" class="btn btn-default btn-raised">Register</a></li>
 		</ul>
 			<?php
 				require_once('templates/main-login.php');
 				require_once('templates/main-register.php');
 			?>
-		<a href="forgot-password">forgot your password?</a>
+		<a class="forgott-password" href="forgot-password">forgot your password?</a>
 	</div>
 	<?php
 	require_once('templates/main-footer.php');
--- comp/templates/main-footer.php
+++ comp/templates/main-footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<ul>
-		<li><a href="http://brolaugh.com">Copyright &copy; 2015 Hannes Kindströmmer</a></li>
-		<li><a href="http://projects.brolaugh.com/codecamp/contact">Contact</a></li>
+		<li class="footer-li"><a href="http://brolaugh.com">Copyright &copy; 2015 Hannes Kindströmmer</a></li>
+		<li class="footer-li"><a href="http://projects.brolaugh.com/codecamp/contact">Contact</a></li>
 	</ul>
 </footer>
