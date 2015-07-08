5dff22c8a923bc8031ca48be621e9fff302ada43
Hannes Kindströmmer <me@brolaugh.com>
2015-07-08 02:50:53 +0200

    Corrected security breach

---
+++ comp/config/config.php
@@ -0,0 +1,17 @@
+<?php
+//This is an example config
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+date_default_timezone_set("Europe/Amsterdam");
+
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASS", "");
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$mysqli->set_charset("utf8");
+if ($mysqli->connect_error) {
+   	echo "Unable to connect to database";
+    exit();
+}
